702dc76020b80a2a51e6ebaaeb9859b6ce2a31a1
Rick Brown <ricky.brown.00@gmail.com>
2019-04-15 17:01:54 +0100

    Updates

---
+++ precache-manifest.a7b9191c9aa35b2f3cf3b94d3ad58232.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a1da08686dd42b03cc2c",
+    "url": "/RickBr0wn/react-redux-todo-app/static/css/main.23f067ef.chunk.css"
+  },
+  {
+    "revision": "a1da08686dd42b03cc2c",
+    "url": "/RickBr0wn/react-redux-todo-app/static/js/main.a1da0868.chunk.js"
+  },
+  {
+    "revision": "269aa1019708910e4f78",
+    "url": "/RickBr0wn/react-redux-todo-app/static/js/1.269aa101.chunk.js"
+  },
+  {
+    "revision": "6d2bddb79bc2dcedeed5",
+    "url": "/RickBr0wn/react-redux-todo-app/static/js/runtime~main.6d2bddb7.js"
+  },
+  {
+    "revision": "52f367524f7e80b75ce7ee449299490a",
+    "url": "/RickBr0wn/react-redux-todo-app/index.html"
+  }
+];
